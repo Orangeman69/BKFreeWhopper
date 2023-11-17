253635c877441fcad44a54871aae928ff4318461
Roland Kertész <roland.kertesz@itsquad.hu>
2023-11-17 23:51:52 +0100

    test3

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 
 from flask import Flask
-# from bk_code import getWhopperCode
+from bk_code import getWhopperCode
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    # whopperCode = getWhopperCode()
-    return "CB1234"
+    whopperCode = getWhopperCode()
+    return whopperCode
 
 if __name__ == '__main__':
     app.run()
