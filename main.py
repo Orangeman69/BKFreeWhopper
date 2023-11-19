ca1ee70a3eeb4a3945c960a96a92f74954e50c0e
Roland Kertész <roland.kertesz@itsquad.hu>
2023-11-19 23:00:10 +0100

    fix api/code return json

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 from bk_code import getWhopperCode
 from dotenv import load_dotenv
 from os import environ
@@ -11,10 +11,10 @@ app = Flask(__name__)
 def root():
     return render_template("index.html")
 
-@app.route('/api/code')
+@app.route('/api/code', methods=['GET'])
 def code():
     whopperCode = getWhopperCode()
-    return whopperCode
+    return jsonify({"code":whopperCode})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3000 if environ.get("MODE") == "dev" else 80)
